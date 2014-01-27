f1e8b0dbd5a3265161f5db6b137bb35810503533
Alex Singleton <alexandersingleton@gmail.com>
2014-01-27 12:57:51 +0000

    Minor changes

--- Code/Open_Atlas_V2.0_Hyperlink.R
+++ Code/Open_Atlas_V2.0_Hyperlink.R
@@ -2,7 +2,10 @@
 setwd("/Volumes/Macintosh HD 2/alex/open-atlas/atlas/")
 
 r_list <- list.files(path=".")
+r_list <- r_list[order(substr(r_list,11,nchar(r_list)))]
+
 r_list_2 <- gsub(".pdf","",r_list)
+
 
 file_info <- file.info(paste("",r_list,sep=''))
 size <- round(file_info$size / 1048576)
@@ -10,16 +13,19 @@ size <- round(file_info$size / 1048576)
 code <- substr(r_list_2,1,9)
 name <- gsub("_"," ",substr(r_list_2,11,nchar(r_list_2)))
 
-#
 
-sink("links.html")
+
+
+
+#MD Version
+
+sink("links.md")
 
 for (i in 1:length(r_list_2)){  
   LAD_Name <- name[i]
   LAD_Code <- code[i]
   url_loc <- paste("http://data.alex-singleton.com/2011_ATLAS/",r_list[i],sep='')
-  html_item <- paste("<a href='",url_loc,"'>",LAD_Code," (",LAD_Name,")"," [",size[i],"MB]</a></br>",sep='')
-  cat(html_item)
+  md_item <- paste("* [",LAD_Code,": ",LAD_Name,"]","(",url_loc,")\n")
+  cat(md_item)
 }
 sink()
-
